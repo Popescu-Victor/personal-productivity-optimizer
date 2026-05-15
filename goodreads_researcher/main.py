34210ae9603d907c32b02d5b17f30eaa4803f4ac
Popescu_Victor <romaniahasmetal@gmail.com>
2026-05-15 20:11:37 +0300

    Path to email login page

--- goodreads_researcher/main.py
+++ goodreads_researcher/main.py
@@ -12,11 +12,30 @@ def obtain_username(handle):
 user_handle = input("Paste the user handle here:   ")
 user_link = obtain_username(user_handle)
 
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+'rating&view=reviews'
+
+
+
+
 def scrape():
 
     driver = webdriver.Edge() 
-    driver.get(user_link)
+    driver.get("https://www.goodreads.com/user/sign_in")
     time.sleep(2)
+
+    login_link = driver.find_element(By.CSS_SELECTOR, ".gr-button.gr-button--dark.gr-button--auth.authPortalConnectButton.authPortalSignInButton")
+    login_link.click()
+    time.sleep(2)
+    
+    driver.get(user_link)
+    
+    time.sleep(2)
+    link = driver.find_element(By.XPATH, "//a[contains(@href, 'rating&view=reviews')]")
+    link.click()
     driver.quit()
 
 scrape()
