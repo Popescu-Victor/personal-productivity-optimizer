40a609ffc55c8a407af724a3d44a69e7b5895f5e
Popescu_Victor <romaniahasmetal@gmail.com>
2026-06-25 08:38:59 +0300

    URL structure

--- cli_functions/webdriver-topage.py
+++ cli_functions/webdriver-topage.py
@@ -4,13 +4,19 @@ from selenium.webdriver.edge.options import Options
 
 
 
+def url_structure(url):
+    if "www." and ".com" in url:
+        return url
+    else:
+        full_url = "www." + str(url) + ".com"
+        return full_url
+
 def open_webpage(url)
     options = Options()
-
     driver = webdriver.Edge(options=options)
-
     driver.get(url)
 
     print("Page title:", driver.title)
 
     driver.quit()
+
