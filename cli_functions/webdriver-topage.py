608f25a6a3c36290434ac61c7f7d7e01bdb98390
Popescu_Victor <romaniahasmetal@gmail.com>
2026-06-25 08:34:26 +0300

    Open webpage from terminal

---
+++ cli_functions/webdriver-topage.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+
+
+
+def open_webpage(url)
+    options = Options()
+
+    driver = webdriver.Edge(options=options)
+
+    driver.get(url)
+
+    print("Page title:", driver.title)
+
+    driver.quit()
